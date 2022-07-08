15614e18e31552ec98c99478c8a8975e2cdc9fd5
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-07-08 17:24:31 +0100

    object model data

---
+++ Back_end/Data/product.DT.ts
@@ -0,0 +1,25 @@
+export enum TypeProduct {
+  shampoing_type = "shampoing",
+  creme_type = "creme",
+  gel_type = "gel",
+  maquillage_type = "maquillage",
+  deodorant_type = "deodorant",
+}
+
+export enum Category {
+  homme_ctg = "homme",
+  femme_ctg = "femme",
+  bebe_ctg = "bebe",
+  soin_ctg = "soin",
+}
+
+export interface CreateProduct {
+  title: string;
+  description: string;
+  product_Type: TypeProduct;
+  category: Category;
+  tag: [string];
+  rating: number;
+  image: [string];
+  price: number;
+}
