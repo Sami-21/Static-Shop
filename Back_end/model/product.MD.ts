3794fe8364a52e90e7ab2e835da805f0ec00d18b
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-07-08 17:25:09 +0100

    product model

---
+++ Back_end/model/product.MD.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { TypeProduct, Category } from "../Data";
+
+interface ProductDoc extends Document {
+  title: string;
+  description: string;
+  product_Type: TypeProduct;
+  category: Category;
+  tag: [string];
+  rating: number;
+  image:[string]
+  price: number;
+}
+
+const ProductSchema = new Schema<ProductDoc>({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  product_Type: {
+    type: String,
+    enum: ["shampoing", "creme", "gel", "maquillage", "deodorant"],
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["homme", "femme", "bebe", "soin"],
+    required: true,
+  },
+  tag: {
+    type: [String],
+  },
+  rating: {
+    type: Number,
+  },
+  image: {
+    type: [String],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const ProductMD = mongoose.model<ProductDoc>("product", ProductSchema);
