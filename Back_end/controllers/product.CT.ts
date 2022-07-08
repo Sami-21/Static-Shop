ded8acf2c79cf8cc01b090a97636693788982175
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-07-09 00:55:01 +0100

    ...

--- Back_end/controllers/product.CT.ts
+++ Back_end/controllers/product.CT.ts
@@ -3,25 +3,23 @@ import { ProductMD } from "../model";
 
 import { CreateProduct } from "../Data";
 export const createProduct = async (req: Request, res: Response) => {
-  const {
-    title,
-    description,
-    product_Type,
-    category,
-    tag,
-    rating,
-    price,
-  } = <CreateProduct>req.body;
+  const { title, description, product_Type, category, tag, rating, price } = <
+    CreateProduct
+  >req.body;
   const files = req.files as [Express.Multer.File];
-  const product = await ProductMD.create({
-    title: title,
-    description: description,
-    product_Type: product_Type,
-    category: category,
-    tag: tag,
-    rating: rating,
-    image: files[0].filename,
-    price: price,
-  });
-  return res.status(200).send(product);
+  try {
+    const product = await ProductMD.create({
+      title: title,
+      description: description,
+      product_Type: product_Type,
+      category: category,
+      tag: tag,
+      rating: rating,
+      image: files[0].filename,
+      price: price,
+    });
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
--- Back_end/services/ExpressApp.ts
+++ Back_end/services/ExpressApp.ts
@@ -1,11 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
-
+import { ProductRouters } from "../routers";
 export default async (app: Application) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.get("/", (req: Request, res: Response) => {
     res.status(200).send("hello world");
   });
+  app.use("/product", ProductRouters);
   return app;
 };
