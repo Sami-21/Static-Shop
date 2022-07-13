32207b4f90e3bb39a0fedaa84b4bbbbc6048250e
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-07-13 16:25:32 +0100

    routers

--- Back_end/routers/product.RT.ts
+++ Back_end/routers/product.RT.ts
@@ -1,8 +1,27 @@
-import express, { Request, Response } from "express";
-import { createProduct } from "../controllers";
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+  getProductID,
+  UpdateProduct,
+  deleteProduct,
+} from "../controllers";
 import { productImage } from "../utility";
 const router = express.Router();
 
+//_____Create new product_____//
 router.post("/create", productImage, createProduct);
+
+//_____Get all products_____//
+router.get("/products", getProducts);
+
+//_____Get product with ID _____//
+router.get("/product/:id", getProductID);
+
+//_____Update product with ID_____//
+router.patch("/product/:id", productImage, UpdateProduct);
+
+//_____Delete product with ID_____//
+router.delete("/product/:id", deleteProduct);
 
 export { router as ProductRouters };
