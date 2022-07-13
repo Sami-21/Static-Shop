6c41ecd863416e4ca530a33e3e7481df1ef7da9e
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-07-13 16:25:01 +0100

    controllers

--- Back_end/controllers/product.CT.ts
+++ Back_end/controllers/product.CT.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { ProductMD } from "../model";
 
-import { CreateProduct } from "../Data";
+import { CreateProduct, Update } from "../Data";
 export const createProduct = async (req: Request, res: Response) => {
   const { title, description, product_Type, category, tag, rating, price } = <
     CreateProduct
@@ -18,8 +18,66 @@ export const createProduct = async (req: Request, res: Response) => {
       image: files[0].filename,
       price: price,
     });
-    return res.status(200).send(product);
+    return res.status(200).json({ product });
   } catch (error) {
     return res.status(400).send(error);
+  }
+};
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await ProductMD.find({});
+    if (products) {
+      return res.status(200).json({ products });
+    }
+  } catch (error) {
+    return res.status(400).send("data not found");
+  }
+};
+
+export const getProductID = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const data = await ProductMD.findById({ _id: id });
+    return res.status(201).json({ status: "successfuly", data });
+  } catch (error: any) {
+    return res.status(401).send(error.message);
+  }
+};
+
+export const UpdateProduct = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const { title, description, product_Type, category, tag, rating, price } = <
+      Update
+    >req.body;
+    const file = req.files as [Express.Multer.File];
+
+    const product = await ProductMD.findByIdAndUpdate(
+      { _id: id },
+      {
+        title: title,
+        description: description,
+        product_Type: product_Type,
+        category: category,
+        tag: tag,
+        rating: rating,
+        image: file[0].filename,
+        price: price,
+      }
+    );
+    return res.status(200).json({ message: "update done" });
+  } catch (error: any) {
+    return res.status(401).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    await ProductMD.findOneAndDelete({ _id: id });
+    return res.status(200).json({ message: "delete done" });
+  } catch (error: any) {
+    return res.status(401).json({ message: error.message });
   }
 };
